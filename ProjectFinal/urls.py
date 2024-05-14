ae3ff4dbc14f428c9267d1ccfbfea97009c71303
Armel03 <94613509+abougma@users.noreply.github.com>
2024-05-14 23:20:10 +0200

    Feat: udapte urls

--- ProjectFinal/urls.py
+++ ProjectFinal/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('moovies/', include('moovies.urls')),
+    path('', include('moovies.urls')),
 ]
